a8b90975c15be46286f03d8c96828f94dfbcaca3
maksim.bugai <maksim.bugai@multiloginapp.com>
2024-02-02 11:14:43 +0200

    isolate pushca client implementation from widget code

--- client/java-script/js/pnotifications.js
+++ client/java-script/js/pnotifications.js
@@ -184,38 +184,3 @@ PushcaClient.openConnection = function (onOpenHandler, onCloseHandler, onMessage
         url: PushcaClient.serverBaseUrl + '/open-connection'
     });
 };
-
-$(document).ready(function () {
-    $('#p-message').val("test message" + Date.now());
-    $("#p-send").click(function () {
-        let filterObj = {};
-        filterObj["workSpaceId"] = "workSpaceMain";
-        filterObj["applicationId"] = "MLA_JAVA_HEADLESS";
-        PushcaClient.broadcastMessage(
-            crypto.randomUUID(),
-            filterObj,
-            true,
-            $('#p-message').val()
-        );
-    });
-
-    PushcaClient.openConnection(
-        function (ws) {
-            PushcaClient.PingIntervalId = window.setInterval(function () {
-                PushcaClient.ws.send(JSON.stringify({"command": "PING"}));
-            }, 20000);
-        },
-        function (ws, event) {
-            window.clearInterval(PushcaClient.PingIntervalId);
-            if (!event.wasClean) {
-                $("#l-message").text("Your connection died, refresh the page please");
-            }
-        },
-        function (ws, event) {
-            if (event.data !== "PONG") {
-                let history = $("textarea#p-history");
-                history.val(history.val() + event.data + "\n");
-            }
-        }
-    );
-});
