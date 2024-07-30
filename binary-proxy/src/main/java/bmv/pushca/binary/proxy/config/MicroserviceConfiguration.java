e31a164eec0beca160e00bab1a6701101baea2b1
maksim.bugai <maksim.bugai@multiloginapp.com>
2024-07-30 08:51:36 +0300

    make all configuration explicit

--- binary-proxy/src/main/java/bmv/pushca/binary/proxy/config/MicroserviceConfiguration.java
+++ binary-proxy/src/main/java/bmv/pushca/binary/proxy/config/MicroserviceConfiguration.java
@@ -6,17 +6,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MicroserviceConfiguration {
 
-  @Value("${api-gateway.response.timeout.ms}")
+  @Value("${binary-proxy.response.timeout.ms}")
   public int responseTimeoutMs;
 
   @Value("${spring.application.name:}")
   public String appName;
 
-  @Value("${api-gateway.selectors-threads-pool.size:8}")
+  @Value("${binary-proxy.selectors-threads-pool.size:8}")
   public int webServerSelectorsPoolSize;
-  @Value("${api-gateway.workers-threads-pool.size:500}")
+  @Value("${binary-proxy.workers-threads-pool.size:500}")
   public int webServerWorkersPoolSize;
-
-  @Value("${binary-proxy.delayed-executor.pool-size:100}")
+  @Value("${binary-proxy.delayed-executor-pool.size:100}")
   public int delayedExecutorPoolSize;
 }
