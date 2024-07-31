a0b12483b8507077c5ad11420e523ef668744999
maksim.bugai <maksim.bugai@multiloginapp.com>
2024-07-31 23:28:20 +0300

    docker image build and service deploy were fixed

--- binary-proxy/build-image.sh
+++ binary-proxy/build-image.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+docker service rm backend-local_binary-proxy
+docker config rm binary-proxy-config-0
+
+docker image rm --force n7fr846yfa6ohlhe/mbugai:binary-proxy-"$1"
+
 mvn clean install -DskipTests
-docker build -t n7fr846yfa6ohlhe/mbugai:binary-proxy-$1 -f Dockerfile .
-docker push n7fr846yfa6ohlhe/mbugai:binary-proxy-$1
+
+docker login -p=sec^GHRmfZu7uTv -u=n7fr846yfa6ohlhe
+docker build -t n7fr846yfa6ohlhe/mbugai:binary-proxy-"$1" -f Dockerfile .
+docker push n7fr846yfa6ohlhe/mbugai:binary-proxy-"$1"
 
 read -p "Press any key..."
--- binary-proxy/docker/init-binary-proxy.sh
+++ binary-proxy/docker/init-binary-proxy.sh
@@ -2,6 +2,6 @@
 
 docker service rm backend-local_binary-proxy
 docker config rm binary-proxy-config-0
-(DEPLOY_VERSION=2 docker stack deploy --with-registry-auth -c backend.yml backend-local)
+(DEPLOY_VERSION=1 docker stack deploy --with-registry-auth -c backend.yml backend-local)
 
 docker service logs -f backend-local_binary-proxy
